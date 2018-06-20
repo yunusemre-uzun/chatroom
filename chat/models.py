0aa6c4b950be20c36120940378038d33a8d57bba
yunusemre-uzun <emreuzun.metu@gmail.com>
2018-06-20 12:50:52 +0300

    asd

--- chat/models.py
+++ chat/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 class Message(models.Model):
     def __str__(self):
-        return self.text + "::" + self.sender + "->" + self.receiver
+        return self.text
     text = models.CharField(max_length=2048)
-    sender = models.CharField(max_length=1024)
-    receiver = models.CharField(max_length=1024)
+    sender = models.CharField(max_length=1024,default="user")
+    receiver = models.CharField(max_length=1024,default="user")
     is_read = models.BooleanField(default=0)
     date = models.DateTimeField('date wrote')
 
--- chat/views.py
+++ chat/views.py
@@ -2,34 +2,12 @@ from django.shortcuts import render
 import datetime
 import time
 
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 from django.shortcuts import render
 from django.utils import timezone
-# Create your views here.
 from django.views import View
-
-'''
-#Gecici deneme classi
-class Message:
-    def __init__(self,messageText,userName,sendingTime):
-        self.messageText = messageText
-        self.userName = userName
-        self.sendingTime = sendingTime
-messages =['Hi','Hello','How are you','Fine,thanks','And you?','Thank you']
-m1 = Message(messages[0],'user1',time.asctime( time.localtime(time.time())))
-time.sleep(1)
-m2 = Message(messages[1],'user2',time.asctime( time.localtime(time.time())))
-time.sleep(1)
-m3 = Message(messages[2],'user1',time.asctime( time.localtime(time.time())))
-time.sleep(1)
-m4 = Message(messages[3],'user2',time.asctime( time.localtime(time.time())))
-time.sleep(1)
-m5 = Message(messages[4],'user2',time.asctime( time.localtime(time.time())))
-time.sleep(1)
-m6 = Message(messages[5],'user1',time.asctime( time.localtime(time.time())))
-messageList = [m1,m2,m3,m4,m5,m6]
-####
-'''
+from .models import Message,User
+from .forms import MessageForm
 
 class IndexView(View):
     def get(self,request):
@@ -38,9 +16,19 @@ class IndexView(View):
 class ChatView(View):
 
     def get(self,request):
-        global messageList
-        context = {'roomName':'1','messageList':messageList}
+        message_list = list(Message.objects.all())
+        form = MessageForm()
+        context = {'roomName':'1','messageList':message_list,'form':form}
         return render(request,'chat/chatroom.html',context)
 
     def post(self, request):
-        return
+        form = MessageForm(request.POST) # A form bound to the POST data
+        if form.is_valid(): # All validation rules pass
+            #print message.cleaned_data['my_form_field_name']
+            print("\nsdkjakjldsk\n")
+            new_message = form.cleaned_data['new_message']
+            message_object = Message(text=new_message,date=timezone.now())
+            message_object.save()
+            HttpResponseRedirect('/chat/') # Redirect after POST
+        #return render(request,'/chat/',)
+        return self.get(request)
