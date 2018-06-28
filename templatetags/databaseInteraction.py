1a05de6ede5eb554045e0c16411e4192c0b9b2ff
yunusemre-uzun <emreuzun.metu@gmail.com>
2018-06-28 10:46:02 +0300

    added latest request to user model, added refreshmessages in databaseinteraction,minor bug fixed in signup view

--- templatetags/databaseInteraction.py
+++ templatetags/databaseInteraction.py
@@ -1,6 +1,7 @@
 from django.db.models import Q
-
 from chat.models import Message
+from django.utils import timezone
+from chat.models import *
 
 
 def getMessages(user_id,receiver_id):
@@ -8,4 +9,23 @@ def getMessages(user_id,receiver_id):
         Q(sender=user_id, receiver=receiver_id) | Q(sender=receiver_id, receiver=user_id)).order_by('-date')[::-1]
 
     return message_list
+
+
+
+def refreshmessages(user_id,receiver_id):
+    new_request = timezone.now()
+    user = MyUser.objects.get(id=user_id)
+    last_request = user.last_request
+    message_list = Message.objects.filter(
+        Q(sender=user_id, receiver=receiver_id) | Q(sender=receiver_id, receiver=user_id)).order_by('-date')[::-1])
+    latest_message = message_list[0]
+    if not(latest_message.date<new_request and latest_message.date>last_request):
+            return []
+    return_message_list = []
+    for message in message_list
+        if(message.date<new_request and message.date>last_request):
+            return_message_list.append(latest_message)
+        else:
+            break
+    return return_message_list
 
